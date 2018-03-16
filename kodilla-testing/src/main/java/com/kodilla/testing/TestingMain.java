41de61ce5eb72f15f0bcf729cc0bdfdb8bc1e26a
Ana9R <A.Burovac@interia.pl>
2018-03-16 21:37:15 +0100

    calculator test

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -1,34 +1,35 @@
 package com.kodilla.testing;
+
 import com.kodilla.testing.calculator.Calculator;
 import com.kodilla.testing.user.SimpleUser;
 
 public class TestingMain {
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
         System.out.println("Test - pierwszy test jednostkowy:");
         SimpleUser simpleUser = new SimpleUser("theForumUser");
         String result = simpleUser.getUsername();
         if (result.equals("theForumUser")) {
             System.out.println("test OK");
-        }else {
+        } else {
             System.out.println("Error!");
         }
         System.out.println("Test - drugi test jednostkowy:");
-        Calculator calculator = new Calculator(5,12);
+        Calculator calculator = new Calculator();
         int result1 = calculator.add(2, 8);
         if (result1 == 10) {
             System.out.println("test OK");
-        }else{
+        } else {
             System.out.println("Error!");
         }
 
         int result2 = calculator.substract(-19, 8);
         if (result2 == -27) {
-        System.out.println("test OK");
-        }else {
+            System.out.println("test OK");
+        } else {
             System.out.println("Error!");
         }
-        }
-        }
+    }
+}
 
 
