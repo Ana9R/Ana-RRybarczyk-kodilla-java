1540b8e2a574db7e69111859f992d9c86aa284c6
Ana9R <A.Burovac@interia.pl>
2018-03-26 23:03:10 +0200

    Beautifull text

--- kodilla-stream/src/main/java/com/kodilla/stream/beautifier/PoemBeautifier.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/beautifier/PoemBeautifier.java
@@ -1,4 +1,9 @@
 package com.kodilla.stream.beautifier;
 
 public class PoemBeautifier {
+
+    public void beautify(String a, PoemDecorator poemDecorator) {
+        String text = poemDecorator.decorate(a);
+        System.out.println("Beautifull text:" + text);
+    }
 }
--- kodilla-stream/src/main/java/com/kodilla/stream/beautifier/StreamMain.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/beautifier/StreamMain.java
@@ -1,4 +1,15 @@
 package com.kodilla.stream.beautifier;
 
+
+import static jdk.nashorn.internal.objects.NativeString.toUpperCase;
+
 public class StreamMain {
+    public static void main(String[] args) {
+        PoemBeautifier poemBeautifier = new PoemBeautifier();
+
+        poemBeautifier.beautify("Krasnoludek", a -> "ABC" + a + "ABC");
+        poemBeautifier.beautify("Krasnoludek", a -> toUpperCase(a));
+        poemBeautifier.beautify("Krasnoludek", a -> "<" + a + "," + a + ">");
+        poemBeautifier.beautify("Krasnoludek", a -> "***" + a + "***");
+    }
 }
