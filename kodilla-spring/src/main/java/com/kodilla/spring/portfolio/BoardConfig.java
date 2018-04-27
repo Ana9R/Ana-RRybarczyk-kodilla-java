8431166cc8aa19f5c1c2e563587649c003acd82d
Ana9R <A.Burovac@interia.pl>
2018-04-27 21:50:04 +0200

    Board task list

--- kodilla-spring/src/main/java/com/kodilla/spring/portfolio/BoardConfig.java
+++ kodilla-spring/src/main/java/com/kodilla/spring/portfolio/BoardConfig.java
@@ -1,13 +1,22 @@
 package com.kodilla.spring.portfolio;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class BoardConfig {
+    @Autowired
+    private TaskList toDoList;
+    @Autowired
+    private TaskList inProgressList;
+    @Autowired
+    private TaskList doneList;
+
     @Bean
     public Board board() {
-        return new Board(toDoList(), inProgressList(), doneList());
+        return new Board(toDoList, inProgressList, doneList);
     }
 
     @Bean
